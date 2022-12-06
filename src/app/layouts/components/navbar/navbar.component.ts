a71c870001ce308dc267c0f219799b3a45f13585
RobinTorres7 <robinhood9607@gmail.com>
2022-12-05 20:40:32 -0600

    get profile3

--- src/app/layouts/components/navbar/navbar.component.ts
+++ src/app/layouts/components/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { TokenStorageService } from 'src/app/_services/token-storage.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'  ]
 })
 export class NavbarComponent implements OnInit {
   user:any; 
